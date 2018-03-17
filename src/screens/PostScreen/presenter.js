70067e1be239af6eaa1daab61853daca0361f81e
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-03-18 06:47:23 +0900

    Add Buttons

--- src/screens/PostScreen/presenter.js
+++ src/screens/PostScreen/presenter.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { View } from "react-native";
+import React, { Component } from "react";
+import { View, TextInput, Button} from "react-native";
 import {
   Container,
   Content,
   List,
   ListItem,
-  Button,
   Icon,
   Text,
 } from 'native-base';
 
-const PostScreen = props => <Container>
+const PostScreen = props =>
+<Container>
         <Content>
           <List>
               <ListItem>
@@ -23,17 +23,53 @@ const PostScreen = props => <Container>
               <Text>Dzxcv</Text>
             </ListItem>
           </List>
-          <Text >Hello</Text>
         </Content>
-        <Button
-          rounded
-          onPress={() => Actions.newPost()}
-        >
-          <Icon
-            name="create"
-            style={{padding: 5}}
-          />
-        </Button>
       </Container>;
 
-export default PostScreen;
+
+
+class UselessTextInput extends Component {
+  render() {
+    return (
+      <TextInput
+        {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+        editable = {true}
+        maxLength = {1000}
+      />
+    );
+  }
+}
+
+export default class UselessTextInputMultiline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Useless Multiline Placeholder',
+    };
+  }
+
+  // If you type something in the text box that is a color, the background will change to that
+  // color.
+  render() {
+    return (
+     <View style={{
+       backgroundColor: '#ffffff',
+       borderBottomColor: '#000000',
+       borderBottomWidth: 1 }}
+     >
+     <UselessTextInput
+       multiline = {true}
+       numberOfLines = {8}
+       onChangeText={(text) => this.setState({text})}
+       value={this.state.text}
+     />
+     <Button
+      title="Learn More"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+      />
+
+     </View>
+    );
+  }
+}
