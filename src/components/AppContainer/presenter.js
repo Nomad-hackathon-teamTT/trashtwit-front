b231f49e08adf2e5e9555cd17e3982d55f334788
YANG YOU SUNG - DevFallingstar <stardung86@gmail.com>
2018-03-18 07:21:20 +0900

    Add geolocation functions

--- src/components/AppContainer/presenter.js
+++ src/components/AppContainer/presenter.js
@@ -7,9 +7,32 @@ import RootNavigation from "../../navigations/RootNavigation";
 
 
 class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
   static propTypes = {
     isLoggedIn: PropTypes.bool.isRequired,
   };
+  componentDidMount() {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            error: null,
+          });
+        },
+        (error) => this.setState({ error: error.message }),
+        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      );
+    }
+
   render() {
     const { isLoggedIn, profile } = this.props;
     return (
@@ -23,6 +46,7 @@ class AppContainer extends Component {
     )
   };
 }
+
 
 export default AppContainer;
 
